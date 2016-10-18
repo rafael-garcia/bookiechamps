4e5875848da372e2c6b99bb8b12c0e389b7c6c64
Rafael Matheus Garcia <rafamg93@gmail.com>
2016-10-18 04:48:11 -0200

    feat(kifi): add importer for kifi's ex-users

---
+++ importers/kifi.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Rafael Matheus Garcia on 18/10/16.
+ */
+
+const kifi = require('../config/config').kifi;
+const _ = require('lodash');
+const db = require('../db/bookieDB');
+
+_.each(kifi.libraries, (lib) => {
+
+});
